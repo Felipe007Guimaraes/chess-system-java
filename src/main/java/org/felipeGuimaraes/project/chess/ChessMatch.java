9498425fd0c43ad96c58c4739663ab7fb64a94f4
Felipe Guimaraes1 <felipe.guimaraes1@ibm.com>
2023-11-21 08:55:41 -0300

    feat: Little improvement in board printing

--- src/main/java/org/felipeGuimaraes/project/chess/ChessMatch.java
+++ src/main/java/org/felipeGuimaraes/project/chess/ChessMatch.java
@@ -28,6 +28,7 @@ public class ChessMatch {
 
     private void InitialSetup(){
         placeNewPiece('a',1,new Rook(board,Color.WHITE));
+        placeNewPiece('a',8,new Rook(board,Color.BLACK));
         placeNewPiece('e',8,new King(board,Color.BLACK));
         placeNewPiece('e',1,new King(board,Color.WHITE));
     }
--- src/main/java/org/felipeGuimaraes/project/exceptions/ChessException.java
+++ src/main/java/org/felipeGuimaraes/project/exceptions/ChessException.java
@@ -1,6 +1,8 @@
 package org.felipeGuimaraes.project.exceptions;
 
 public class ChessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
     public ChessException(String msg){
         super(msg);
     }
